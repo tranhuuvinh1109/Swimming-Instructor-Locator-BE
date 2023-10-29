45ed2fb2b97a88d8435549c9c4533a67a5c8298d
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-10-29 09:04:09 +0700

    / send

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const UserRoute = require("./UserRoute");
 const TeacherRoute = require("./TeacherRoute");
 const AuthRoute = require("./AuthRoute");
+const RootRoute = require("./RootRoute");
 
 function routes(app) {
+  app.use("/", RootRoute);
   app.use("/user", UserRoute);
   app.use("/teacher", TeacherRoute);
   app.use("/auth", AuthRoute);
